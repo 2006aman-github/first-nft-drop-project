7b3ba3c617d8a5f8e3598e21dd8e672a3c031ab3
Aman Chaubey <amanjames122@gmail.com>
2022-04-17 16:23:42 +0530

    added connect and disconnect functionality more ui updates and icons. Added footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,10 @@ import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { sanityClient, urlFor } from '../sanity'
 import { Collection } from '../typings'
+import { Icon } from '@iconify/react'
 
 interface Props {
   collections: Collection[]
@@ -15,7 +16,11 @@ const Home = ({ collections }: Props) => {
   const connectWithMetamask = useMetamask()
 
   const disconnect = useDisconnect()
+
   const address = useAddress()
+
+  // function to show random bubbles
+
   return (
     <div className="m-auto flex min-h-screen  flex-col">
       <Head>
@@ -23,52 +28,66 @@ const Home = ({ collections }: Props) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {/* <h1 className="font-bold text-red-500">Welcome to the NFT Project</h1> */}
-      <main className="min-h-screen bg-black">
+      <main className="z-0 min-h-screen  bg-black/90">
         {/* view-1  */}
-
         <div className="flex h-screen flex-col">
+          {/* floating bubbles of traits of nft-collection  */}
+          {/* <img src={'/assets/svg/bubble-1.svg'} ref={ref1 as React.LegacyRef<HTMLImageElement>} className='w-20 -z-10 hidden animate-wiggle  absolute top-1/2 left-1/2' alt="" />
+          <img src={'/assets/svg/bubble-2.svg'} className='w-20 -z-10 bg-black/10 animate-wiggle  absolute top-1/4 right-1/3 -z-2' alt="" />  
+            <img src={'/assets/svg/bubble-3.svg'} className='w-20 -z-10 animate-wiggle bg-black/10  absolute top-1/2 left-2/3 -z-2' alt="" /> 
+               <img src={'/assets/svg/bubble-4.svg'} className='w-20 -z-10 animate-wiggle bg-black/10  absolute top-1/6 left-1/2 -z-2' alt="" />  
+                <img src={'/assets/svg/bubble-5.svg'} className='w-20 -z-10 bg-black/10 animate-wiggle  absolute top-1/4 left-1/8 -z-2' alt="" /> 
+                   <img src={'/assets/svg/bubble-6.svg'} className='w-20 -z-10 bg-black/10 animate-wiggle  absolute top-1/5 right-5/3 -z-2' alt="" /> */}
           {/* header  */}
           <header className="flex-.1  flex justify-end  p-5 px-5 align-baseline">
             {address ? (
-              <div className="rounded-full border-2 border-slate-400 px-5 py-2 text-white">
-                {address.substring(0, 5) +
-                  '...' +
-                  address.substring(address.length - 5)}
+              <div className="flex items-center justify-center space-x-2 rounded-full border-2 border-slate-700 px-5 py-2 text-white">
+                <span>
+                  <Icon icon="bx:wallet-alt" />
+                </span>
+                <span className="text-white">
+                  {address.substring(0, 5) +
+                    '...' +
+                    address.substring(address.length - 5)}
+                </span>
               </div>
             ) : (
               <button
                 onClick={() => (address ? disconnect() : connectWithMetamask())}
-                className="text-bold rounded-full bg-indigo-600 px-5 py-2 text-center text-white"
+                className="text-bold flex items-center justify-center space-x-2 rounded-full bg-indigo-600 px-5 py-2 text-center text-white transition-all duration-100 hover:outline hover:outline-4 hover:outline-indigo-500"
               >
-                Connect to a wallet
+                <span>Connect to a wallet</span>
+                <Icon icon="clarity:connect-solid" rotate={1} />
               </button>
             )}
           </header>
           {/* body  */}
           <div className="w-100 flex flex-auto flex-col items-center justify-center space-y-7  font-bold">
-            <h1 className=" w-50  text-center text-4xl text-indigo-600 lg:text-7xl">
+            <h1 className=" w-50 text-center text-3xl  text-indigo-600 sm:text-5xl md:text-6xl lg:text-7xl">
               Welcome to, Aman-Tech
             </h1>
-            <h3 className="w-100 mb-5 text-2xl font-bold text-white lg:text-5xl">
+            <h3 className="w-100 mb-5 text-2xl font-bold text-white md:text-3xl lg:text-5xl">
               NFT Marketplace
             </h3>
-            <p className="w-full px-5 text-center text-sm text-slate-300 lg:px-3 lg:text-xl">
+            <p className="w-full px-5 text-center text-xs text-slate-300 sm:text-sm lg:px-3 lg:text-xl">
               Explore My Collection of NFTs and start investing in one of the
               cheapest NFT collections on the internet
             </p>
             {address ? (
               <button
                 onClick={() => disconnect()}
-                className="text-bold mt-3 rounded-full bg-slate-200 px-5 py-2 text-center text-black transition-all duration-100 hover:outline hover:outline-4 hover:outline-indigo-500"
+                className="text-bold mt-3 flex items-center justify-center space-x-2 rounded-full bg-slate-200 px-5 py-2 text-center text-black transition-all duration-100 hover:outline hover:outline-4 hover:outline-indigo-500"
               >
-                Disconnect wallet
+                <span>Disconnect wallet</span>
+                <Icon icon="codicon:debug-disconnect" width="16" height="16" />
               </button>
             ) : (
               <button
                 onClick={() => (address ? disconnect() : connectWithMetamask())}
-                className="text-bold mt-3 rounded-full bg-indigo-600 px-5 py-2 text-center text-white transition-all duration-100 hover:outline hover:outline-4 hover:outline-indigo-500"
+                className="text-bold mt-3 flex items-center justify-center space-x-2 rounded-full bg-indigo-600 px-5 py-2 text-center text-white transition-all duration-100 hover:outline hover:outline-4 hover:outline-indigo-500"
               >
-                Connect to a wallet
+                <span>Connect to a wallet</span>
+                <Icon icon="clarity:connect-solid" rotate={1} />
               </button>
             )}
           </div>
@@ -79,13 +98,19 @@ const Home = ({ collections }: Props) => {
             There you go!
           </h1>
           {/* collection-body  */}
-          <div className="flex w-fit  items-center justify-center rounded-xl  bg-slate-900/40 p-10">
+          <div className="flex w-fit  items-center justify-center rounded-xl  bg-slate-900/90 p-10">
             <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
               {collections.map((collection) => (
                 <Link href={`/nft/${collection.slug.current}`}>
-                  <div className="flex h-60 w-40 cursor-pointer flex-col rounded-xl bg-white p-1 lg:h-80 lg:w-60">
+                  <div className="group relative flex h-80 w-60 cursor-pointer flex-col rounded-xl bg-white p-1">
                     <img
-                      className="h-30 mb-2 w-40 rounded-xl object-cover lg:h-60 lg:w-60"
+                      src={'/assets/svg/arrow.svg'}
+                      className="absolute top-3
+                     right-3 bg-transparent duration-300 group-hover:right-2 group-hover:top-2"
+                      width="30"
+                    />
+                    <img
+                      className="h-30 mb-2 w-60 rounded-xl object-cover "
                       src={urlFor(collection.previewImage).url()}
                       alt=""
                     />
@@ -98,6 +123,60 @@ const Home = ({ collections }: Props) => {
             </div>
           </div>
         </div>
+        {/* footer  */}
+        <footer className=" mb-1">
+          {/* my social media  */}
+          <div className="flex justify-center">
+            <div className="mb-2 flex space-x-5 text-white">
+              <Link href={'https://www.facebook.com/aman.chaubey.3597789/'}>
+                <Icon
+                  fontSize={'1rem'}
+                  icon="bi:meta"
+                  className="cursor-pointer duration-100 hover:-translate-y-1"
+                  width="40"
+                  height="40"
+                />
+              </Link>
+              <Link href={'https://www.instagram.com/aaaman_cha/'}>
+                <Icon
+                  icon="akar-icons:instagram-fill"
+                  className="cursor-pointer duration-100 hover:-translate-y-1"
+                  width="40"
+                  height="40"
+                />
+              </Link>
+              <Link href={'https://www.linkedin.com/in/aman-james-7aa6a0201/'}>
+                <Icon
+                  icon="akar-icons:linkedin-box-fill"
+                  className="cursor-pointer duration-100 hover:-translate-y-1"
+                  width="40"
+                  height="40"
+                />
+              </Link>
+              <Link href={'https://twitter.com/AmanCha09928971'}>
+                <Icon
+                  icon="akar-icons:twitter-fill"
+                  className="cursor-pointer duration-100 hover:-translate-y-1"
+                  width="40"
+                  height="40"
+                />
+              </Link>
+              <Link href={'https://github.com/2006aman-github'}>
+                <Icon
+                  icon="akar-icons:github-fill"
+                  className="cursor-pointer duration-100 hover:-translate-y-1"
+                  width="40"
+                  height="40"
+                />
+              </Link>
+            </div>
+            <div></div>
+          </div>
+          {/* copyright  */}
+          <p className="text-center text-xs text-white">
+            Copyright © Aman Chaubey 2022. All Rights Reserved
+          </p>
+        </footer>
       </main>
     </div>
   )
